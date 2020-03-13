a59eb414ea9676138cb1cb2d96734344f1f851c7
Addi Amaya <chris.amaya.caa@gmail.com>
2020-03-13 11:39:14 -0600

    image_preperation_task function stub for image-processing_task.c

---
+++ main-application/image-processing_tasks.c
@@ -0,0 +1,16 @@
+def image_preperation_task():
+    /**
+    * @brief
+    *
+    *
+    * @header
+    * @param
+    * @pre
+    * @post
+    * @return
+    */
+    while(1){
+
+    
+    }
+    return
