6bd9476295f80bd8416407148da447d93713a2c2
Addi Amaya <chris.amaya.caa@gmail.com>
2020-03-13 11:43:30 -0600

    Added some comments for image_preperation_task function

--- main-application/image-processing_tasks.c
+++ main-application/image-processing_tasks.c
@@ -1,16 +1,17 @@
-def image_preperation_task():
+def image_preperation_task(image, SRAM):
     /**
-    * @brief
-    *
-    *
-    * @header
-    * @param
-    * @pre
-    * @post
-    * @return
+    * @brief prepare the image for processing
+    * @header software-and-command/main-application
+    * @param image - is the image that will be processes
+    *        SRAM - is the where in the storage the image will be stored
+    * @pre None
+    * @post Stores the image into the memory storage
+    * @return None
     */
     while(1){
+        // At this point do we just call the functions that
+        // were made for us by the image processing team?
 
-    
+        // Carter can you check if our doxygen docstring makes sense
     }
     return
