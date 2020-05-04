b160a7b762e4b1c07c296c845d3cda67fad42b0b
Addi Amaya <chris.amaya.caa@gmail.com>
2020-05-03 19:06:53 -0600

    task stubs for capture task and image preperation task

--- main-application/image-processing_tasks.c
+++ main-application/image-processing_tasks.c
@@ -1,72 +1,68 @@
-void imagePreperationTask(imageID, SRAM, verison):
+void imagePreperationTask(void* pvParameters){
     /**
     * @brief prepare the image for processing
+    *
+    * This task will grab a full image from the cubesense memory and initalize
+    * image download and check the results. If we want the image to be a thumbnail
+    * it will go through a quality check. The end result will give a compressed file
+    * that will be prepared and sent to the downlink manager.
     * @header software-and-command/main-application
-    * @param imageID - is the image that will be processes
-    *        SRAM - is the where in the storage the image will be stored
-    *        version - thumbnail == 0, full photo == 1
+    * @param parameters unused
     * @pre None
     * @post Stores the image into the memory storage
     * @return None
     */
     while(1){
-        if version == 0:
-            {
-            // find a way to reference the image from the cubesense memory
-            // maybe have capture task return the location?
-            imageDownload = initalizeImageDownload(32frames)
-             if checkDownloadResults(imageDownload):
-                {
-                if qualityCheck(imageDownload):
-                    {
-                    compressed = compressThumbnail(imageDownload)
-                    prepDownlink(imageDownload)
-                    }
+        const char *pcTaskName = "imagePreperationTask is running\r\n"
+        uint8_t version = version(); // version is afunction we assumed will return which version we want from a telecommand
+        uint8_t frameT = 32; // frames, telecommand ID 64
+        uint8_t frameI = 8192; // frames, telecommand ID 64
+        uint32_t img = cubesenseMemory(); // would this exist?
+        // for the cubesenseMemory function it would have to be faster then then
+        // periodic trigger on the capture Task
+        uint8_t imageDownload;
+        uint8_t compressed
+        if version == 0{    // thumbnail version
+            imageDownload = initalizeImageDownload(frameT, img)
+            if checkDownloadResults(imageDownload){
+                if qualityCheck(imageDownload){
+                    compressed = compress(imageDownload)
+                    prepDownlink(compressed)
                 }
             }
+        }
 
-        if version == 1:
-            {
-            // find a way to reference the image from the cubesense memory
-            // maybe have capture task return the location?
-            imageDownload = initalizeImageDownload(8192frames)
-            if checkDownloadResults(imageDownload):
-                compressed = compressThumbnail(imageDownload)
-                prepDownlink(imageDownload)
+        if version == 1{    // Full image version
+            imageDownload = initalizeImageDownload(frameI, img)
+            if checkDownloadResults(imageDownload){
+                compressed = compress(imageDownload)
+                prepDownlink(compressed)
             }
+        }
     }
-    return 0
+}
 
 
 
-void captureTask():
+void captureTask(){
     /**
     * @brief Image capture task
+    *
+    * This task will get called from a periodic trigger. After checking if the
+    * camera is ready to be used, it will take the photo storing it in one of the
+    * local SRAM memory locations.
     * @header software-and-command/main-application
-    * @param None
+    * @param pararmeters unused
     * @pre None
     * @post Store one image into the Cubesense memory
     * @return None
     */
     while(1){
-    uint_8 cameraReadyCheck()
-    if cameraReadyCheck == 1 //assuming 1 is True (Ready)
-        img = captureImage(SRAM) // where do we put the captured image in the cubesense memory
+    const char *pcTaskName = "captureTask is running\r\n";
+    uint_8 ready = cameraReadyCheck();
+        if ready == 1{ //assuming 1 is True (Ready)
+            captureImage(SRAM)
+        }
 
     }
-    return 0
-
-    // This will have to return the location of the photo in the cubesense memory
-    // because we call in the imagePreperationTask
-
-
-
-    // discarded notes and code: ----------------------------------------------------------
-    // uint_8 img = retrieveImage(imageID) // assuming retrieveImage dereferences for us
-    // uint_8 compressed = compression(img) // idk what the compression means entirely -Addi
-    // uint_8 prepared = prepDownlink(compressed) // turns it to packets?
-    // // somehow send it to downlink manager
-    //     // is this suppose to even be in a while(1)?
-    // // we dont know what the downlink manager is expecting? so how do we send it?
-    // downLinkManager(prepared)
-    // // Downlink manager isn't where its supposed to go i think
+}
