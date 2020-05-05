f044cefa639b165565a5caa11e8a9b696067a4d8
mpbtruong <billtruongphong@hotmail.com>
2020-05-04 21:48:18 -0600

    deparserTask function stub in comms-processing_tasks.c

---
+++ main-application/comms-processing_tasks.c
@@ -0,0 +1,34 @@
+/*  comms-processing_tasks.c
+
+
+    Created by Minh Phong Bill Truong on April 28th 2020
+ */
+
+#include <stdio.h>
+#include "comms-processing_tasks.h"
+
+//==============================================================================
+//                                   GLOBALS
+//==============================================================================
+
+
+//==============================================================================
+//                                  FUNCTIONS
+//==============================================================================
+
+/**
+ * 
+ *  @header	"software-and-command/main-applicatoion/comms-processing_tasks.c"
+ *	@param	
+ *	@pre	
+ * 	@post	
+ * 	@return	
+ */
+void deparseTask( ) {
+    //TODO: get the actual function names when available
+
+    while(1) {
+ 
+    }
+
+}
