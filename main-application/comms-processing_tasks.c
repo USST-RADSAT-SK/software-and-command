9f1763e0924ceaae88e8d630a4c9a271dcc4b11d
mpbtruong <billtruongphong@hotmail.com>
2020-05-20 17:22:14 -0600

    Initial commit for deparseTask stub

--- main-application/comms-processing_tasks.c
+++ main-application/comms-processing_tasks.c
@@ -1,8 +1,9 @@
 /*  comms-processing_tasks.c
 
-
     Created by Minh Phong Bill Truong on April 28th 2020
  */
+#include "FreeRTOS.h"
+#include "semphr.h"
 
 #include <stdio.h>
 #include "comms-processing_tasks.h"
@@ -17,18 +18,26 @@
 //==============================================================================
 
 /**
+ *  @brief Call functions required to deparse xxx
+ *  
+ *  The deparser task receives the data that is to be sent down (clarify) and calls the
+ *  required functions to handle the formatting of the data into a packet. 
  * 
  *  @header	"software-and-command/main-applicatoion/comms-processing_tasks.c"
- *	@param	
- *	@pre	
- * 	@post	
- * 	@return	
+ *	@param	Unused
+ *	@pre	None
+ * 	@post	Formatted xxx is stored in memory
+ * 	@return	None
  */
-void deparseTask( ) {
-    //TODO: get the actual function names when available
-
+void deparseTask(void* pvparameters) {
     while(1) {
- 
+        /*
+        * Wait until signal to deparse xxx is given
+        */
+        if (xSemaphoreTake(deparseSignal, portMAX_DELAY)){
+            deparseDataTask(void* pvparameters); // Stub function call
+            encodeTask(void* pvparameters); // Stub function call
+        }
     }
-
+    return 0;
 }
