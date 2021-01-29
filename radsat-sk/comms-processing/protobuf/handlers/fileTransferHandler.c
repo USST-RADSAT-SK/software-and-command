646fedf9b066dac431d6846dabb06a4896a09876
tyrelkostyk <tck290@mail.usask.ca>
2021-01-28 19:07:52 -0600

    extended fileTransferHandler functions. WIP. TCK290

--- radsat-sk/comms-processing/protobuf/handlers/fileTransferHandler.c
+++ radsat-sk/comms-processing/protobuf/handlers/fileTransferHandler.c
@@ -1,37 +1,47 @@
-///*
-// * fileTransferHandler.c
-// *
-// *  Created on: Jan 25, 2021
-// *      Author: Ty
-// */
-//
-//#include <protobuf.h>
-//
-//#include "hal/Drivers/I2C.h"
-//
-//
-//void fileTransferHandle(void)
-//{
-//	transferResponse message;
-//
-//	message.packetNumber = 10;
-//	message.packetType = 0;
-//	message.responseType = ACK;
-//
-////	MyMessage.message = message;
-//
-//	uint8_t *outgoingBuffer = (uint8_t *)malloc(transferResponse_size);
-//	// ...
-//
-//	protoEncode((uint8_t *)message, sizeof(outgoingBuffer), outgoingBuffer);
-//
-//	// TODO: replace with more generic (custom) I2C call
-//	I2C_write(0x4F, outgoingBuffer, sizeof(outgoingBuffer));
-//}
-//
-//
-//typedef enum response_ {
-//	ACK = 0,
-//	NACK = 1,
-//} response_t;
-//
+/*
+ * fileTransferHandler.c
+ *
+ *  Created on: Jan 25, 2021
+ *      Author: Ty
+ */
+
+#include <protobuf.h>
+
+#include "hal/Drivers/I2C.h"
+
+
+void fileTransferReceive(uint16_t messageId)
+{
+
+	switch (messageId) {
+
+	// the response (ACK, etc.) to a file transfer packet
+	case (fileTransferMessage_FileTransferResponse_tag):
+		break;
+
+	// a file transfer packet
+	case (fileTransferMessage_FileTransferPacket_tag):
+		break;
+
+	// an unknown message
+	default:
+		assert(false);
+		break;
+	}
+
+}
+
+
+void fileTransferSend(uint16_t messageId)
+{
+	uint8_t *outgoingMessage = (uint8_t *)malloc(fileTransferMessage_size);
+	assert(outgoingMessage);
+	memset(outgoingMessage, 0, fileTransferMessage_size);
+
+
+	protoEncode((uint8_t *)outgoingMessage, sizeof(outgoingBuffer), outgoingBuffer);
+
+	// TODO: replace with more generic (custom) radsat-sk I2C call
+	I2C_write(0x4F, outgoingBuffer, sizeof(outgoingBuffer));
+}
+
