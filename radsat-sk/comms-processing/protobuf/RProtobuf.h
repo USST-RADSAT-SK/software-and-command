9916ba2ebb3d41e760563b45e36e4d957b933884
tyrelkostyk <tck290@mail.usask.ca>
2021-03-27 11:48:00 -0600

    updated value for max proto message, and removed the header since somethign else may be used. TCK

--- radsat-sk/comms-processing/protobuf/RProtobuf.h
+++ radsat-sk/comms-processing/protobuf/RProtobuf.h
@@ -18,16 +18,7 @@
                                              DEFINITIONS
 ***************************************************************************************************/
 
-#define PROTO_MAX_ENCODED_SIZE	((uint16_t)256)		///> The max size of a message. TODO: Update with real value
-#define PROTO_PREAMBLE			((uint16_t)0x2005)	///> The year the USST was founded :)
-
-typedef struct _RProtoHeader {
-	uint16_t preamble;		///> A 16 bit flag that defines the "start" of any protobuf message
-	uint16_t size;			///> The size of the message in bytes
-	uint32_t crc;			///> A 4-byte (32-bit) CRC of the message, not including the header
-	uint16_t topicTag;		///> The ID of the topic for which the message belongs to
-	uint16_t messageTag;	///> The ID of the message itself
-} RProtoHeader;
+#define PROTO_MAX_ENCODED_SIZE	((uint16_t)RadsatMessage_size)	///> The max size of a message
 
 
 /***************************************************************************************************
