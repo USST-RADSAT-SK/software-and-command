c3e21256d7dee42a430a9602dc2ebdb49f52323b
Addi Amaya <addi.amaya99@gmail.com>
2021-04-12 20:41:01 -0600

    updated the header file

--- radsat-sk/src/tasks/RImageCaptureTask.h
+++ radsat-sk/src/tasks/RImageCaptureTask.h
@@ -1,1 +1,13 @@
+/**
+ * @file RImageCaptureTask.h
+ * @date April 12, 2021
+ * @author Addi Amaya
+ */
+
+#ifndef RIMAGECAPTURETASK_H_
+#define RIMAGECAPTURETASK_H_
+
+
+
+#endif /* RIMAGECAPTURETASK_H_ */
 
