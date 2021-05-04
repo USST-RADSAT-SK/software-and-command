e3e67088091165fcbc031543ffd5e6fc1dc1f930
Tyrel Kostyk <tck290@mail.usask.ca>
2021-05-03 20:47:37 -0600

    ported in NanoPB (#21)
    * refactored project directoru structure. Much cleaner now :)
    
    * changed README to reflect my suggestions for an updated coding style; remove overuse of whitespace, curly bois on newline after function def; added info on doxygen documentation; updated branch naming patterns
    
    * bleh
    
    * added framework and operation folders to resources to build
    
    * ported in NanoPB
    
    * updated README after discussion with oligarchs
    
    * changes to README
    
    * updated githooks and README
    
    * updated README
    
    * updated gitignore
    
    * removed deprecrated doxygen comment
    
    * fixed coding standard compliance
    
    * simplified return in protoEncode
    
    * made unions anonymous; made protoEncode return size of encoded message if successful. TCK
    
    Co-authored-by: Tyrel <tkostyk@bourgault.com>

---
+++ radsat-sk/operation/message/protobuf/RProtobuf.c
@@ -0,0 +1,56 @@
+/**
+ * @file RProtobuf.c
+ * @date January 25, 2021
+ * @author Tyrel Kostyk (tck290)
+ */
+
+#include <RProtobuf.h>
+#include <assert.h>
+
+
+/***************************************************************************************************
+                                             PUBLIC API
+***************************************************************************************************/
+
+/**
+ * Encode a raw protobuf message with NanoPB into a buffer, and encode the header as well.
+ *
+ * @param rawMessage The raw (unencoded) RadSat struct message to encode.
+ * @param outgoingBuffer The buffer that will hold the encoded message & header.
+ * @return The size of the encoded message; 0 if encoding failed.
+ */
+uint8_t protoEncode(RadsatMessage* rawMessage, uint8_t* outgoingBuffer) {
+	// ensure incoming buffer is not NULL
+	assert(rawMessage);
+	assert(outgoingBuffer);
+
+	// create stream object
+	pb_ostream_t stream = pb_ostream_from_buffer((uint8_t*)rawMessage, PROTO_MAX_ENCODED_SIZE);
+
+	// encode the message into the byte array
+	if (pb_encode(&stream, RadsatMessage_fields, &outgoingBuffer))
+		return stream.bytes_written;
+
+	// if the encoding failed, return 0
+	return 0;
+}
+
+
+/**
+ * Decode an encoded protobuf message, and confirm that the contents match the header.
+ *
+ * @param incomingBuffer The buffer containing the encoded message/header.
+ * @param decodedMessage The message that will be populated with the decoded message.
+ * @return true if the message is decoded successfully, false otherwise.
+ */
+uint8_t protoDecode(uint8_t* incomingBuffer, RadsatMessage* decodedMessage) {
+	// ensure incoming buffer is not NULL
+	assert(incomingBuffer);
+	assert(decodedMessage);
+
+	// create stream object
+	pb_istream_t stream = pb_istream_from_buffer((uint8_t*)incomingBuffer, PROTO_MAX_ENCODED_SIZE);
+
+	// encode the message into the byte array
+	return pb_decode(&stream, RadsatMessage_fields, &decodedMessage);
+}
