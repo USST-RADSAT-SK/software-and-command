272603616f012bb45d117ad3c12846aae0c26d33
Jacob Waskowic <jaw352@mail.usask.ca>
2021-05-05 20:11:21 -0600

    Implement UART rx and tx wrappers - JAW352

--- radsat-sk/framework/wrappers/RUart.c
+++ radsat-sk/framework/wrappers/RUart.c
@@ -1,37 +1,34 @@
 /**
  * @file RUart.c
  * @date May 3, 2021
- * @author Addi Amaya (caa746)
+ * @author Addi Amaya (caa746) & Jacob Waskowic (jaw352)
  */
 
+
+#include "RUart.h"
 
 /***************************************************************************************************
                                              PUBLIC API
 ***************************************************************************************************/
 
+
 /**
- *
- *
+ * Sends the given data over UART
+ * @param data: A pointer to the data to send over UART
+ * @param size: The number of bytes to be sent
+ * @return An integer error code. 0 indicates success
  */
-
-uint8_t UART_tx(){
-	//enable UART
-
-	//UART Write
+int UART_tx(const unsigned char* data, unsigned int size) {
+	return UART_write(bus0_uart, data, size);
 }
 
 
 /**
- *
- *
+ * Receives data over UART and stores it in the given buffer
+ * @param data: A buffer to store the received data in
+ * @param size: The number of bytes to recieve over UART
+ * @return An integer error code. 0 indicates success
  */
-
-uint8_t UART_rx(){
-	//enable UART
-
-	//enables RX
-	//Check if RX was successful
-
-	//UART read
-
+int UART_rx(unsigned char* data, unsigned int size) {
+	return UART_read(bus0_uart, data, size);
 }
--- radsat-sk/framework/wrappers/RUart.h
+++ radsat-sk/framework/wrappers/RUart.h
@@ -1,12 +1,15 @@
 /**
  * @file RUart.h
- * @date
- * @author
+ * @date May 5, 2021
+ * @author Addi Amaya (caa746) & Jacob Waskowic (jaw352)
  */
 
 #ifndef RUART_H_
 #define RUART_H_
 
+#include "hal/Drivers/UART.h"
 
+int UART_tx(const unsigned char* data, unsigned int size);
+int UART_rx(unsigned char* data, unsigned int size);
 
 #endif /* RUART_H_ */
