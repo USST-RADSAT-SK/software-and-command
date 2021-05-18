443c223f849b638589a80acccefdb523240341f3
Addi Amaya <addi.amaya99@gmail.com>
2021-05-17 20:17:02 -0600

    Fixed RUART header to have more definitions, double checked usage of UART.h usage

--- radsat-sk/framework/wrappers/RUart.h
+++ radsat-sk/framework/wrappers/RUart.h
@@ -7,11 +7,13 @@
 #ifndef RUART_H_
 #define RUART_H_
 
-#include "hal/Drivers/UART.h"
+#include "includes/hal/Drivers/UART.h"
 
+#define AT91C_US_USMODE_NORMAL 0
 #define CAMERA_BAUD_RATE 57600
 #define TIME_GUARD 0
 #define RX_TIMEOUT 255
+#define TRUE 1
 
 int UART_blockingTransmit(const uint8_t* data, uint16_t size);
 int UART_blockingTransmit(uint8_t* data, uint16_t size);
