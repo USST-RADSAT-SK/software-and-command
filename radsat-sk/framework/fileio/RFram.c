e09694f2c47e4f60cc3db017f612d96cd004bcc0
tyrelkostyk <tck290@mail.usask.ca>
2021-05-18 21:00:38 -0600

    added first revision of the FRAM wrapper. Might be the final rev, for now

---
+++ radsat-sk/framework/fileio/RFram.c
@@ -0,0 +1,44 @@
+/**
+ * @file RFram.c
+ * @date May 18 2021
+ * @author Tyrel Kostyk
+ */
+
+#include <RFram.h>
+#include <hal/Storage/FRAM.h>
+
+
+/***************************************************************************************************
+                                             PUBLIC API
+***************************************************************************************************/
+
+/**
+ * @brief Initialize the FRAM peripheral driver.
+ */
+void framInit(void) {
+	FRAM_start();
+}
+
+
+/**
+ * @brief Read data out of the FRAM peripheral.
+ * @param data The pointer to where the extracted data will be copied.
+ * @param address The FRAM address to begin extracting data from.
+ * @param size The number of bytes to copy out of the FRAM peripheral.
+ * @return 0 for success, below 0 for failure. See hal/Storage/FRAM.h for more details.
+ */
+int16_t framRead(uint8_t* data, uint32_t address, uint32_t size) {
+	return FRAM_read(data, address, size);
+}
+
+
+/**
+ * @brief Write data into the FRAM peripheral.
+ * @param data The pointer to where the data will be copied from.
+ * @param address The FRAM address to begin writing data to.
+ * @param size The number of bytes to copy into the FRAM peripheral.
+ * @return 0 for success, below 0 for failure. See hal/Storage/FRAM.h for more details.
+ */
+int16_t framWrite(uint8_t* data, uint32_t address, uint32_t size) {
+	return FRAM_writeAndVerify(data, address, size);
+}
--- radsat-sk/framework/fileio/RFram.h
+++ radsat-sk/framework/fileio/RFram.h
@@ -1,12 +1,23 @@
 /**
  * @file RFram.h
- * @date
- * @author
+ * @date May 18 2021
+ * @author Tyrel Kostyk
  */
 
 #ifndef RFRAM_H_
 #define RFRAM_H_
 
+#include <stdint.h>
+
+
+/***************************************************************************************************
+                                             PUBLIC API
+***************************************************************************************************/
+
+void framInit(void);
+
+int16_t framRead(uint8_t* data, uint32_t address, uint32_t size);
+int16_t framWrite(uint8_t* data, uint32_t address, uint32_t size);
 
 
 #endif /* RFRAM_H_ */
