bba1a01e95462ff9edcd45fa7ecb7fe86530c1d3
Julian <ngp143@usask.ca>
2021-05-22 15:45:46 -0600

    Created header stub for the SD-header file

---
+++ radsat-sk/framework/wrappers/RSDcard.h
@@ -0,0 +1,21 @@
+/**
+ * @file RUart.h
+ * @date May 5, 2021
+ * @author Addi Amaya (caa746) & Jacob Waskowic (jaw352)
+ */
+
+#ifndef RUART_H_
+#define RUART_H_
+
+#include <hal/Drivers/UART.h>
+
+#define CAMERA_BAUD_RATE 57600
+#define TIME_GUARD 0
+#define RX_TIMEOUT 255
+
+
+uint32_t UART_blockingTransmit(const uint8_t* data, uint16_t size);
+uint32_t UART_blockingRecieve(uint8_t* data, uint16_t size);
+uint32_t UART_init();
+
+#endif /* RUART_H_ */
