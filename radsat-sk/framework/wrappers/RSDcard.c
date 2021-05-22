299777659d79a2ff29d2bb9b28bc2f258bb01ec7
Addi Amaya <addi.amaya99@gmail.com>
2021-05-22 15:33:20 -0600

    just made stubs and commented them out. awaiting PR from add-hcc-library

---
+++ radsat-sk/framework/wrappers/RSDcard.c
@@ -0,0 +1,35 @@
+/**
+ * @file RSDcard.c
+ * @date May 22, 2021
+ * @author Addi Amaya (caa746) Julian Pham (ngp143)
+ *
+ */
+
+//#include <hcc/api_fat.h>
+//#include <hcc/api_hcc_mem.h>
+//#include <hcc/api_fat_test.h>
+//#include <hcc/api_mdriver_atmel_mcipdc.h>
+//
+//#include <hal/Utility/fprintf.h>
+//
+//#include <freertos/FreeRTOS.h>
+//#include <freertos/task.h>
+//
+//#include <at91/utility/assert.h>
+//#include <stdint.h>
+
+/***************************************************************************************************
+                                             PUBLIC API
+***************************************************************************************************/
+
+//uint32_t SD_init(uint16_t volID){
+//	break;
+//}
+//
+//uint32_t SD_read(uint8_t filename){
+//	break;
+//}
+//
+//uint32_t SD_write(uint8_t filename){
+//	break;
+//}
