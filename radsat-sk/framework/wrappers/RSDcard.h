28558754503c082d7a6ccfbd2b0715953ef33d98
Addi Amaya <addi.amaya99@gmail.com>
2021-05-29 17:27:07 -0600

    added includes to header file

--- radsat-sk/framework/wrappers/RSDcard.h
+++ radsat-sk/framework/wrappers/RSDcard.h
@@ -7,6 +7,23 @@
 #ifndef RSDCARD_H_
 #define RSDCARD_H_
 
+#include <hcc/api_fat.h>
+#include <hcc/api_hcc_mem.h>
+#include <hcc/api_fat_test.h>
+#include <hcc/api_mdriver_atmel_mcipdc.h>
+
+#include <hal/Utility/fprintf.h>
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#include <at91/utility/assert.h>
+#include <stdint.h>
+
+/***************************************************************************************************
+                                            DEFINITIONS
+***************************************************************************************************/
+
 
 uint32_t SD_init(uint16_t volID);
 
