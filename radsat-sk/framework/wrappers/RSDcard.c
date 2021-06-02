bce397bdedd5a5a08c7ea9dfdcf78a28917410f4
Addi Amaya <addi.amaya99@gmail.com>
2021-06-02 14:36:28 -0600

    initialized SD_init and added _SAFE definition in header

--- radsat-sk/framework/wrappers/RSDcard.c
+++ radsat-sk/framework/wrappers/RSDcard.c
@@ -26,14 +26,13 @@
 	#define DEMO_SD_TRACE_FATAL		TRACE_FATAL
 #endif
 
-#define _SAFE 1
 #define _BUFF_SIZE ( 2 * 1024 )
 
 unsigned char pWriteBuff[ _BUFF_SIZE ];
 unsigned char pReadBuff[ _BUFF_SIZE ];
 
-static void _TaskTestFileLogWrite( void * pvParam );
-static void _TaskTestFileLogRead( void * pvParam );
+static void _TaskTestFileLogWrite( void* pvParam );
+static void _TaskTestFileLogRead( void* pvParam );
 static void _MemSetTestPattern( unsigned char * pBuffer, unsigned int size );
 
 static xTaskHandle _task[2] = {0};
@@ -44,6 +43,36 @@ static xTaskHandle _task[2] = {0};
  * write and read operations. For a complete list of available filesystem
  * commands refer to the user guide.
  */
+
+void SD_init( uint16_t volID) {
+	uint16_t ret;
+
+	if( volID != 0 && volID != 1 ) {
+		return;
+	}
+
+	hcc_mem_init(); /* Initialize the memory to be used by filesystem */
+
+	//check
+	ret = fs_init(); /* Initialize the filesystem */
+	ASSERT( (ret == F_NO_ERROR), "fs_init pb: %d\n\r", ret);
+	ret = f_enterFS(); /* Register this task with filesystem */
+	ASSERT( (ret == F_NO_ERROR), "f_enterFS pb: %d\n\r", ret);
+
+	#if(_SAFE)
+		ret = f_initvolume(0, atmel_mcipdc_initfunc, volID); /* Initialize volID as safe */
+	#else
+		ret = f_initvolume_nonsafe(0, atmel_mcipdc_initfunc, volID); /* Initialize volID as nonsafe */
+	#endif
+	ASSERT( ((ret == F_NO_ERROR) || (ret == F_ERR_NOTFORMATTED)), "f_initvolume pb: %d\n\r", ret);
+
+	if(F_ERR_NOTFORMATTED == ret){
+		ret = f_format( 0, F_FAT32_MEDIA ); /* Format the filesystem */
+		ASSERT( (ret == F_NO_ERROR), "f_format pb: %d\n\r", ret);
+	}
+
+}
+
 void sdBasic( uint16_t volID ){
 	uint16_t ret, i, bw, br;
 	F_FILE* file;
