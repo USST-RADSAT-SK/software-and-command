1631a05c8f5312e0ae3e64b29ae2e8171ad51ab6
Addi Amaya <addi.amaya99@gmail.com>
2021-06-08 23:31:20 -0600

    finished draft of header file, theres a TODO to finish

--- radsat-sk/framework/wrappers/RI2C.h
+++ radsat-sk/framework/wrappers/RI2C.h
@@ -6,9 +6,23 @@
  *
  */
 
+
+#ifndef RI2C_H_
+#define RI2C_H_
+
+
 #include <hal/Drivers/I2C.h>
 
 //TODO: Determine a speed for the i2c
 #define I2C_BUSSPEED_HZ 200000
 #define I2C_TRANSFERTIMEOUT 0
 #define WRITEREAD_DELAY 0
+
+uint32_t i2cInit();
+uint32_t i2cBlockingTransmit(uint16_t slaveAddress, const uint8_t* data, uint16_t size);
+uint32_t i2cBlockingRecieve(uint16_t slaveAddress, uint8_t* data, uint16_t size);
+uint32_t i2cBlockingRxTx(uint16_t slaveAddress, uint16_t writeSize, uint16_t readSize, uint8_t* writeData, volatile uint8_t* readData);
+uint32_t i2cQueue(uint16_t slaveAddress, uint16_t writeSize, uint16_t readSize, uint8_t* writeData, volatile uint8_t* readData);
+
+
+#endif /* RI2C_H_ */
