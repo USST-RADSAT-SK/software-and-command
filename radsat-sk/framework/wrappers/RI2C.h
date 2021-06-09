ff5ad0848940fa8539f9d6a8ea05c3bf6c600faf
Addi Amaya <addi.amaya99@gmail.com>
2021-06-09 14:28:10 -0600

    added Doxygen comments

--- radsat-sk/framework/wrappers/RI2C.h
+++ radsat-sk/framework/wrappers/RI2C.h
@@ -18,10 +18,71 @@
 #define I2C_TRANSFERTIMEOUT 0
 #define WRITEREAD_DELAY 0
 
+
+/***************************************************************************************************
+                                             FUNCTION DECLARATIONS
+***************************************************************************************************/
+
+/**
+ * Initializes the I2C driver
+ *
+ * @return 0 for success, otherwise review I2C.h for return definition
+ */
 uint32_t i2cInit();
+
+/**
+ * Writes data to a slave using DMA
+ *
+ * @note this is a blocking call
+ * @pre i2cInit must be successful
+ * @param slaveAddress address of the slave where to make the transfer
+ * @param data Memory location of the data to be written
+ * @param size Number of bytes to be written to the I2C slave
+ * @return 0 for success, otherwise review I2C.h for return definition
+ */
 uint32_t i2cBlockingTransmit(uint16_t slaveAddress, const uint8_t* data, uint16_t size);
+
+/**
+ * Reads data from a slave
+ *
+ * @note this is a blocking call
+ * @pre i2cInit must be successful
+ * @param slaveAddress Address of the slave where to make the transfer.
+ * @param data Memory location to store the data read from the I2C slave. Must be able to store size bytes.
+ * @param size Number of bytes to be read from the I2C slave.
+ * @return 0 for success, otherwise review I2C.h for return definition
+ */
 uint32_t i2cBlockingRecieve(uint16_t slaveAddress, uint8_t* data, uint16_t size);
+
+/**
+ * Writes and then reads data to/from a slave on the I2C
+ *
+ * @note If tx->readSize is zero but tx->writeSize is non-zero, this becomes a write transfer.
+ * Conversely, If tx->writeSize is zero but tx->readSize is non-zero, this becomes a read transfer.
+ * @note this is a blocking call
+ * @pre i2cInit must be successful
+ * @param slaveAddress Address of the slave where to make the transfer.
+ * @param writeSize Number of bytes to be written to the I2C slave.
+ * @param readSize Number of bytes to be read from the I2C slave
+ * @param writeData Memory location of the data to be written to the I2C slave
+ * @param readData Memory location to store the data read from the I2C slave
+ * @return 0 for success, otherwise review I2C.h for return definition
+ */
 uint32_t i2cBlockingRxTx(uint16_t slaveAddress, uint16_t writeSize, uint16_t readSize, uint8_t* writeData, volatile uint8_t* readData);
+
+/**
+ * Queues a transfer into the I2C driver
+ *
+ * @note this is a non-blocking call
+ * @pre i2cInit must be successful
+ * @param slaveAddress Address of the slave where to make the transfer.
+ * @param writeSize Number of bytes to be written to the I2C slave.
+ * @param readSize Number of bytes to be read from the I2C slave
+ * @param writeData Memory location of the data to be written to the I2C slave
+ * @param readData Memory location to store the data read from the I2C slave
+ * @return 0 for success, otherwise review I2C.h for return definition
+ *
+ */
 uint32_t i2cQueue(uint16_t slaveAddress, uint16_t writeSize, uint16_t readSize, uint8_t* writeData, volatile uint8_t* readData);
 
 
--- radsat-sk/framework/wrappers/RI2Cslave.h
+++ radsat-sk/framework/wrappers/RI2Cslave.h
@@ -15,9 +15,38 @@
 
 #define TIMEOUT_DELAY 0
 
-
+/**
+ * initializes I2C Slave driver
+ *
+ * @note see I2CSlaveCommandList for more details on commands
+ * @param address Address of the slave on the I2C bus.
+ * @param command Command code (first byte sent by the I2C master)
+ * @param commandParamSize Number of bytes to read after receiving the command. If the command does not have any parameters, this should be 0
+ * @param commandListSize Number of commands
+ * @return 0 for success, otherwise review I2C.h for return definition
+ *
+ */
 uint32_t i2cInitSlave(uint8_t address, uint8_t command, uint16_t commandParamSize, uint16_t commandListSize);
+
+/**
+ * Writes data TO the master
+ *
+ * @pre i2cInitSlave must be successful
+ * @param data Pointer to a location where the data to write is stored.
+ * @param size Number of bytes to transfer
+ * @return 0 for success, otherwise review I2Cslave.h for return definition
+ *
+ */
 uint32_t i2cSlaveTx(const uint8_t* data, uint16_t size);
+
+/**
+ * Reads data from the I2C bus
+ *
+ * @note This function simply waits until actual data is available and returns when the i2c master sends a command
+ * @param data A pointer to the location where the driver should store the received data
+ * @return The number of bytes received as a command
+ *
+ */
 uint32_t i2cSlaveRx(uint8_t* data);
 
 #endif /* RI2CSLAVE_H_ */
