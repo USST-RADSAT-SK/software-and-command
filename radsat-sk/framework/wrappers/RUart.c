f68209c89e961d50b489c2c98907bef11232bc0a
Matthew Buglass <60230689+matthew-buglass@users.noreply.github.com>
2021-10-28 21:23:47 -0600

    Removed "Blocking" from the names of uartBlocking transmit and receive (#59)
    * Changed the function name uartBlockTransmit to uartTransmit
    
    * Changed the function name uartBlockingReceive to uartReceive
    
    * Added error handling to Receive and Transmit
    
    Added error handling to uartReceive and uartTransmit
    
    Co-authored-by: mab839 <mab839@usask.ca>

--- radsat-sk/framework/wrappers/RUart.c
+++ radsat-sk/framework/wrappers/RUart.c
@@ -20,8 +20,15 @@ UARTconfig cameraConfig = {AT91C_US_USMODE_NORMAL | AT91C_US_CLKS_CLOCK | AT91C_
  * @param size: The number of bytes to be sent
  * @return An integer error code. 0 indicates success
  */
-uint32_t uartBlockingTransmit(const uint8_t* data, uint16_t size) {
-	return UART_write(bus0_uart, data, size);
+uint32_t uartTransmit(const uint8_t* data, uint16_t size) {
+	int err = UART_write(bus0_uart, data, size);
+	
+	if(err == 0) {
+		return err;
+	}
+	else {
+		return 1;
+	}
 }
 
 
@@ -31,8 +38,15 @@ uint32_t uartBlockingTransmit(const uint8_t* data, uint16_t size) {
  * @param size: The number of bytes to receive over UART
  * @return An integer error code. 0 indicates success
  */
-uint32_t uartBlockingReceive(uint8_t* data, uint16_t size) {
-	return UART_read(bus0_uart, data, size);
+uint32_t uartReceive(uint8_t* data, uint16_t size) {
+	int err = UART_read(bus0_uart, data, size);
+	
+	if(err == 0) {
+		return err;
+	}
+	else {
+		return 1;
+	}
 }
 
 /**
--- radsat-sk/framework/wrappers/RUart.h
+++ radsat-sk/framework/wrappers/RUart.h
@@ -17,8 +17,8 @@
                                              FUNCTION DECLARATIONS
 ***************************************************************************************************/
 
-uint32_t uartBlockingTransmit(const uint8_t* data, uint16_t size);
-uint32_t uartBlockingReceive(uint8_t* data, uint16_t size);
+uint32_t uartTransmit(const uint8_t* data, uint16_t size);
+uint32_t uartReceive(uint8_t* data, uint16_t size);
 uint32_t uartInit();
 
 #endif /* RUART_H_ */
