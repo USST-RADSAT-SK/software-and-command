bd54f305bfba3f3e588e33fd6eeb8203b8706a1c
Matthew Buglass <60230689+matthew-buglass@users.noreply.github.com>
2021-11-28 20:50:23 -0600

    updated docs
    I just specified explicitly that 1 was the failure code

--- radsat-sk/framework/wrappers/RSPI.c
+++ radsat-sk/framework/wrappers/RSPI.c
@@ -15,7 +15,7 @@
  * Initiates SPI communications
  * @param bus: The SPI bus we are using on the OBC
  * @param spi1_max_cs: The maximum number of chip selects on the OBC
- * @return An integer error code. 0 indicates success
+ * @return An integer error code. 0 indicates success, 1 for failure
  */
 
 uint32_t spiInit(SPIbus bus, SPIslave spi1_max_cs) {
@@ -34,7 +34,7 @@ uint32_t spiInit(SPIbus bus, SPIslave spi1_max_cs) {
 /**
  * Sends and Recieves data over SPI
  * @param tx: SPItransfer structure specifying the details of the tasks
- * @return An integer error code. 0 indicates success
+ * @return An integer error code. 0 indicates success, 1 for failure
  */
 
 uint32_t spiTransmitRecieve(SPItransfer *tx){
