d4dcde490d3a20fde131800d36b6708c5fd1d4fc
Shiva Moghtaderi <moghtaderishiva@gmail>
2021-12-23 13:50:35 -0600

    creatd the camera header file

---
+++ radsat-sk/operation/subsystems/RCamera.h
@@ -0,0 +1,30 @@
+/**
+ * @file RCamera.h
+ * @date December 23, 2021
+ * @author Shiva Moghtaderi (shm153)
+ */
+
+#ifndef RCAMERA_H_
+#define RCAMERA_H_
+
+/***************************************************************************************************
+                                            DEFINITIONS
+***************************************************************************************************/
+
+typedef enum _camera_image_slots {
+	image_slot_one,
+	image_slot_two,
+	image_slot_three,
+	sun_sensor_slot_one,
+	camera_slot_count,
+} camera_slot_t;
+
+
+/***************************************************************************************************
+                                             PUBLIC API
+***************************************************************************************************/
+
+int cameraCaptureImage(camera_slot_t slot);
+
+
+#endif /* RCAMERA_H_ */
