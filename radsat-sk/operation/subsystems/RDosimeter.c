8a8b4bbf45b6e00a53552643ab4b0dbfeef431f3
tyrelkostyk <tck290@mail.usask.ca>
2021-12-28 18:16:07 -0600

    First implementation of actual dosimeter code; replaced pseudocode with real code that compiles

--- radsat-sk/operation/subsystems/RDosimeter.c
+++ radsat-sk/operation/subsystems/RDosimeter.c
@@ -1,111 +1,148 @@
 /**
  * @file RDosimeter.c
- * @date February 11, 2020
- * @author Tyrel Kostyk
+ * @date December 28, 2021
+ * @author Tyrel Kostyk (tck290) and Isaac Poirier (iap992)
  */
 
-#ifdef DONT_COMPILE_THE_FOLLOWING_CODE
+#include <RDosimeter.h>
+#include <RI2c.h>
+#include <string.h>
+#include <stdint.h>
 
-// TODO: update this code to use real code instead of pseudocode
 
-
-//==============================================================================
-//                                   GLOBALS
-//==============================================================================
+/***************************************************************************************************
+                                            DEFINITIONS
+***************************************************************************************************/
 
 /**
- *  The slave addresses of the two Melanin-Dosimeter boards.
- *	TODO: confirm once actual addresses are determined
+ * Reference voltage for the Melanin-Dosimeter boards. Sensors return relative
+ * 8-bit values. E.g. 127 out of 255 would be 50%, representing 1.65V
  */
-uint8_t dosimeterBoardSlaveAddr[2] = {
+#define DOSIMETER_REFERENCE_VOLTAGE	((float)3.30)
+
+/** I2C Slave Address for Dosimeter Board One */
+#define DOSIMETER_1_I2C_SLAVE_ADDR	(0x4A)
+/** I2C Slave Address for Dosimeter Board Two */
+#define DOSIMETER_2_I2C_SLAVE_ADDR	(0x48)
+/** I2C Response for Dosimeter Board readings are 12 bits (2 bytes) long */
+#define DOSIMETER_I2C_DELAY			(0)
+
+/** I2C Response for Dosimeter Board readings are 12 bits (2 bytes) long */
+#define DOSIMETER_COMMAND_LENGTH			(1)
+/** I2C Response for Dosimeter Board readings are 12 bits (2 bytes) long */
+#define DOSIMETER_RESPONSE_LENGTH			(2)
+/** High byte of 12-bit readings must have top 4 bits masked away */
+#define DOSIMETER_RESPONSE_HIGH_BYTE_MASK	(0x0F)
+
+/** ADC takes 12-bit readings */
+#define ADC_BIT_RESOLUTION	(12)
+/** Used for converting ADC values to real voltage readings */
+#define MAX_ADC_VALUE		((float) (1 << 12))
+
+
+/***************************************************************************************************
+                                          PRIVATE GLOBALS
+***************************************************************************************************/
+
+/** The two Melanin-Dosimeter boards. */
+enum _dosimeterBoards {
+	dosimeterBoardOne,
+	dosimeterBoardTwo,
+	dosimeterBoardCount,
+};
+
+/** The two Melanin-Dosimeter boards. */
+enum _adcChannels {
+	adcChannelZero,		// COTS 2.048V REF IC, Shielding: none
+	adcChannelOne,		// COTS 2.048V REF IC, Shielding: 50 mil
+	adcChannelTwo,		// COTS 2.048V REF IC, Shielding: 100 mil
+	adcChannelThree,	// COTS 2.048V REF IC, Shielding: 200 mil
+	adcChannelFour,		// COTS 2.048V REF IC, Shielding: 20 mil
+	adcChannelFive,		// RADFET Experimental Dosimeter, Shielding: none
+	adcChannelSix,		// COTS 2.048V REF IC, Shielding: 300 mil
+	adcChannelSeven,	// Temperature Sensor
+	adcChannelCount,
+};
+
+/** The slave addresses of the two Melanin-Dosimeter boards. */
+static uint8_t dosimeterBoardSlaveAddr[dosimeterBoardCount] = {
 	DOSIMETER_1_I2C_SLAVE_ADDR,
 	DOSIMETER_2_I2C_SLAVE_ADDR
 };
 
-
 /**
- *  Byte commands to request data from each of the 8 channels on each
+ *  Byte commands to request data conversions for each of the 8 channels on each
  *	Melanin-Dosimeter board. See datasheet for details.
  */
-uint8_t dosimeterCommandBytes[8] = {
-	0x84,     // 0 - 1000.0100
-	0xC4,     // 1 - 1100.0100
-	0x94,     // 2 - 1001.0100
-	0xD4,     // 3 - 1101.0100
-	0xA4,     // 4 - 1010.0100
-	0xE4,     // 5 - 1110.0100
-	0xB4,     // 6 - 1011.0100
-	0xF4      // 7 - 1111.0100
+static uint8_t dosimeterCommandBytes[adcChannelCount] = {
+	0x84,     // Channel 0 - 1000.0100
+	0xC4,     // Channel 1 - 1100.0100
+	0x94,     // Channel 2 - 1001.0100
+	0xD4,     // Channel 3 - 1101.0100
+	0xA4,     // Channel 4 - 1010.0100
+	0xE4,     // Channel 5 - 1110.0100
+	0xB4,     // Channel 6 - 1011.0100
+	0xF4      // Channel 7 - 1111.0100
 };
 
 
-//==============================================================================
-//                                  FUNCTIONS
-//==============================================================================
+/***************************************************************************************************
+                                             PUBLIC API
+***************************************************************************************************/
 
-/** @brief Request and store readings from all Melanin-Dosimeter channels.
+/**
+ * Request and store readings from all Melanin-Dosimeter channels.
  *
- *	Iterates through each channel on both boards to collect and store payload
- *	data from the experimental Melanin-Dosimeter boards. Relies on asynchronous
- *	I2C calls to send commands and receive the information in a response.
+ * Iterates through each channel on both boards to collect and store payload
+ * data from the experimental Melanin-Dosimeter boards. Relies on I2C calls to
+ * send commands and receive the information in a response.
  *
- *	@header	"software-and-command/payload_processing/payload_processing.h"
- *	@param	void
- *	@pre	Melanin-Dosimeter boards are powered on. No initialization necessary
- * 	@post	Payload data is stored in non-volatile memory (TODO: location TBD)
- * 	@return	void
+ * @pre I2C must be initialized
+ * @post If successful, sends new formatted group of readings to the Downlink Manager
+ * @param void
+ * @return Returns 0 on Success, anything else indicates error.
  */
-void requestReadingsAllChannels( void )
+int requestReadingsAllChannels(void)
 {
+	uint8_t dataResponse[DOSIMETER_RESPONSE_LENGTH] = { 0 };
+
 	// iterate through both melanin-dosimeter boards
-	for ( uint8_t dosimeterBoard = 0; dosimeterBoard < DOSIMETER_COUNT; dosimeterBoard++ ) {
+	for (uint8_t dosimeterBoard = dosimeterBoardOne; dosimeterBoard < dosimeterBoardCount; dosimeterBoard++) {
 
-    	// request data from each sensor on a particular board
-		for ( uint8_t adcChannel = 0; adcChannel < 8; adcChannel++ ) {
+		// request data from each sensor on a particular board
+		for (uint8_t adcChannel = adcChannelOne; adcChannel < adcChannelCount; adcChannel++) {
 
-			// write command to dosimeter board using i2c
-			uint8_t i2cResult = i2c->writeByte( dosimeterBoardSlaveAddr[dosimeterBoard],
-												dosimeterCommandBytes[adcChannel]
-											  	);
+			// reset internal buffer
+			memset(dataResponse, 0, DOSIMETER_RESPONSE_LENGTH);
+
+			// tell dosimeter to begin conversion; receive 12-bit data into our internal buffer
+			int error = i2cTalk(dosimeterBoardSlaveAddr[dosimeterBoard], DOSIMETER_COMMAND_LENGTH,
+								DOSIMETER_RESPONSE_LENGTH, &dosimeterCommandBytes[adcChannel],
+								dataResponse, DOSIMETER_I2C_DELAY);
 
 			// check for success of I2C command
-			if ( i2cResult != 0 ) {
-				// 0 = success code; anything else is a failure
-				// TODO: replace assertion with logging and proper error handling
-				ASSERT("ERROR");
-			}
+			if ( error != 0 )
+				return error;
 
-			// TODO: Do we need to add any delays? Don't think so, but should confirm
-
-			// send the recieve command over i2c to dosimeter board; store return values
-			// high byte contains bits 11-8 (most signifigant), low contains bits 7-0
-			// TODO: modify in the future to work with real I2C commands
-			uint8_t conversionResultHighByte = i2c->readByte( dosimeterBoardSlaveAddr[dosimeterBoard] );
-			uint8_t conversionResultLowByte = i2c->readByte( dosimeterBoardSlaveAddr[dosimeterBoard] );
-
-			// check for zero values in response (values should never be zero)
-			if ( conversionResultHighByte == 0 ) {
-				// reading should never be 0
-				// TODO: replace assertion with logging and proper error handling
-				ASSERT("WARNING");
-			}
-			else if ( conversionResultLowByte == 0 ) {
-				// reading should never be 0
-				// TODO: replace assertion with logging and proper error handling
-				ASSERT("WARNING");
-			}
+			// extract the high (sent first) and low (sent second) data bytes from the I2C response
+			// high byte (top 4 bits of 12-bit value) must be masked & bit-shifted
+			uint8_t conversionResultHighByte = ((dataResponse[0] & DOSIMETER_RESPONSE_HIGH_BYTE_MASK) << 8);
+			uint8_t conversionResultLowByte = dataResponse[1];
 
 			// combine high and low values
-			uint16_t conversionResultTotal = ( ( resultByteHigh & 0x0F ) << 8 ) + resultByteLow;
+			uint16_t conversionResultTotal = conversionResultHighByte + conversionResultLowByte;
 
-			// scale conversion result to final voltage value
-			float voltageResult = DOSIMETER_REFERENCE_VOLTAGE * ( conversionResultTotal / 255.0 )
+			// scale conversion result (based on reference voltage)
+			// TODO: figure out what our external ADC reference voltage is
+			float voltageResult = DOSIMETER_REFERENCE_VOLTAGE * ( conversionResultTotal / MAX_ADC_VALUE );
 
-			// store final voltage result in memory
-			storePayload( dosimeterBoard, adcChannel, voltageResult );
-    }
-  }
+			// TODO: format data into a Protobuf message
+
+			// TODO: send formatted messages to downlink manager
+
+		}
+	}
+
+	return 0;
 }
-
-#endif	// DONT_COMPILE_THE_FOLLOWING_CODE
 
