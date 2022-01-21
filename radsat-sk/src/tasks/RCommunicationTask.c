26ab76059d71104e24fd349f7f746fd7e29b9bdf
mab839 <mab839@usask.ca>
2022-01-21 17:22:12 -0600

    Added skeleton for transmitter and receiver tasks. Added private functions for handling the pass timer. The callback function is empty becasue I haven't defined the structs for the tasks yet.

--- radsat-sk/src/tasks/RCommunicationTask.c
+++ radsat-sk/src/tasks/RCommunicationTask.c
@@ -7,21 +7,26 @@
 #include <RTransceiver.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <freertos/timers.h>
 
-int passtime = 0;
+#define PASS_TIMER_ID 1
+#define MAX_PASS_LENGTH 240000 // 4 minutes in ms
+
+static int passtime = 0;
+static xTimerHandle passTimer;
 
 /***************************************************************************************************
                                        PRIVATE FUNCTION STUBS
 ***************************************************************************************************/
 
 static void startPassMode(void);
-
+static void vCallbackFunction( xTimerHandle timer );
 
 /***************************************************************************************************
 											 PUBLIC API
 ***************************************************************************************************/
 
-void taskCommunication(void* parameters)
+void receiverTask(void* parameters)
 {
 	(void)parameters;
 
@@ -29,48 +34,57 @@ void taskCommunication(void* parameters)
 
 	while(1) {
 
-		// continue receiving frames while they're available
-		uint16_t framesReady = 0;
-		error = transceiverRxFrameCount(&framesReady);
-		while(error == 0 && framesReady > 0) {
-			// let system know we've entered a pass
-			startPassMode();
 
-			// allocate a buffer
-			uint8_t message[TRANCEIVER_RX_MAX_FRAME_SIZE];
+		vTaskDelay(1);
+	}
+}
 
-			// retrieve a message
-			uint16_t sizeOfMessage = 0;
-			error = transceiverGetFrame(message, &sizeOfMessage);
-			if (error)
-				break;
+void transmitterTask(void* parameters)
+{
+	(void)parameters;
 
-			// TODO: stuff...
+	int error = 0;
 
-			// update frame size for loop condition
-			error = transceiverRxFrameCount(&framesReady);
-		}
-
-		/*
-		while (downlinkFrameCount > 0) {
-
-		}
-
-		*/
-
-		// done uplinking & downlinking; end passtime
-		passtime = 0;
+	while(1) {
 
 		vTaskDelay(1);
 	}
 }
 
 
+/***************************************************************************************************
+                                         PRIVATE FUNCTIONS
+***************************************************************************************************/
+
+/**
+ * Callback function for the pass timer that resets structs to a neutral state in preparation for the next pass
+ *
+ * @param timer A handle for a timer. However this is implicitly called and is passed without parameters
+ * 				to xTimerCreate()
+ */
+static void vCallbackFunction( xTimerHandle timer ) {
+
+}
+
+/**
+ * Starts a timer for the pass timeout
+ */
 static void startPassMode(void) {
 	if (passtime == 0) {
 		passtime = 1;
-		//xTimerCreate(12min, endPassMode);
+
+		if (passTimer == NULL) {
+			// If the timer was not created yet, create it
+			passTimer = xTimerCreate("passTimer", MAX_PASS_LENGTH, pdFALSE, PASS_TIMER_ID, vCallbackFunction);
+			xTimerStart(passTimer, 0);
+		}
+		else {
+			// Otherwise restart it
+			xTimerReset(passTimer, 0);
+		}
 	}
 }
+
+
 
 
