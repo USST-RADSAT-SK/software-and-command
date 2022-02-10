6a567ca5a3a2735acc5820622f4bb428fa9a2bc5
tyrelkostyk <tck290@mail.usask.ca>
2022-02-09 22:09:54 -0600

    fixed a few potential bugs in the protobuf module

--- radsat-sk/operation/message/protobuf/RProtobuf.c
+++ radsat-sk/operation/message/protobuf/RProtobuf.c
@@ -20,15 +20,16 @@
  * @return The size of the encoded message; 0 if encoding failed.
  */
 uint8_t protoEncode(RadsatMessage* rawMessage, uint8_t* outgoingBuffer) {
-	// ensure incoming buffer is not NULL
-	assert(rawMessage);
-	assert(outgoingBuffer);
+
+	// ensure incoming buffers are not NULL
+	if (rawMessage == 0 || outgoingBuffer == 0)
+		return 0;
 
 	// create stream object
 	pb_ostream_t stream = pb_ostream_from_buffer((uint8_t*)rawMessage, PROTO_MAX_ENCODED_SIZE);
 
 	// encode the message into the byte array
-	if (pb_encode(&stream, RadsatMessage_fields, &outgoingBuffer))
+	if (pb_encode(&stream, RadsatMessage_fields, outgoingBuffer))
 		return stream.bytes_written;
 
 	// if the encoding failed, return 0
@@ -44,13 +45,14 @@ uint8_t protoEncode(RadsatMessage* rawMessage, uint8_t* outgoingBuffer) {
  * @return true if the message is decoded successfully, false otherwise.
  */
 uint8_t protoDecode(uint8_t* incomingBuffer, RadsatMessage* decodedMessage) {
-	// ensure incoming buffer is not NULL
-	assert(incomingBuffer);
-	assert(decodedMessage);
+
+	// ensure incoming buffers are not NULL
+	if (incomingBuffer == 0 || decodedMessage == 0)
+		return 0;
 
 	// create stream object
 	pb_istream_t stream = pb_istream_from_buffer((uint8_t*)incomingBuffer, PROTO_MAX_ENCODED_SIZE);
 
 	// encode the message into the byte array
-	return pb_decode(&stream, RadsatMessage_fields, &decodedMessage);
+	return pb_decode(&stream, RadsatMessage_fields, decodedMessage);
 }
