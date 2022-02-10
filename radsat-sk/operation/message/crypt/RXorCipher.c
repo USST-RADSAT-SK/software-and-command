1f49142df16f8a3a1e625a3d514fac5d2e972285
tyrelkostyk <tck290@mail.usask.ca>
2022-02-09 22:33:19 -0600

    removed tiny-AES library and custom software; created module for XOR Cipher. Updated include paths as necessary

---
+++ radsat-sk/operation/message/crypt/RXorCipher.c
@@ -0,0 +1,7 @@
+/**
+ * @file RXorCipher.c
+ * @date February 9, 2022
+ * @author Tyrel Kostyk (tck290)
+ */
+
+#include <RXorCipher.h>
---
+++ radsat-sk/operation/message/crypt/RXorCipher.h
@@ -0,0 +1,13 @@
+/**
+ * @file RXorCipher.h
+ * @date February 9, 2022
+ * @author Tyrel Kostyk (tck290)
+ */
+
+
+#ifndef RXORCIPHER_H_
+#define RXORCIPHER_H_
+
+
+
+#endif /* RXORCIPHER_H_ */
