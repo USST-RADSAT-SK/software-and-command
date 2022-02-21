af685e51903a39ead23134434f1d7d784bb2a0a7
tyrelkostyk <tck290@mail.usask.ca>
2022-02-21 12:16:25 -0600

    created protocol service module (added services source folder)

---
+++ radsat-sk/operation/services/RProtocolService.c
@@ -0,0 +1,7 @@
+/**
+ * @file RProtocolService.c
+ * @date February 21, 2022
+ * @author Tyrel Kostyk (tck290)
+ */
+
+#include <RProtocolService.h>
---
+++ radsat-sk/operation/services/RProtocolService.h
@@ -0,0 +1,13 @@
+/**
+ * @file RProtocolService.h
+ * @date February 21, 2022
+ * @author Tyrel Kostyk (tck290)
+ */
+
+#ifndef RPROTOCOLSERVICE_H_
+#define RPROTOCOLSERVICE_H_
+
+#include <RProtocol.pb.h>
+
+
+#endif /* RPROTOCOLSERVICE_H_ */
