f31eaf7519cfc2892ae4c1a03f73e3fbbb6dd84b
Isaac Poirier <iap992@mail.usask.ca>
2022-03-15 14:02:00 -0600

    initial code documenting structure for both RObc.c and RObc.h

---
+++ radsat-sk/operation/subsystems/RObc.c
@@ -0,0 +1,8 @@
+/*
+ * RObc.c
+ *
+ *  Created on: Mar 10, 2022
+ *      Author: isaac
+ */
+
+
---
+++ radsat-sk/operation/subsystems/RObc.h
@@ -0,0 +1,13 @@
+/*
+ * RObc.h
+ *
+ *  Created on: Mar 10, 2022
+ *      Author: isaac
+ */
+
+#ifndef ROBC_H_
+#define ROBC_H_
+
+
+
+#endif /* ROBC_H_ */
