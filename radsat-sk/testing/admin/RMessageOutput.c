bd1c05915fbc7590d39b24aa83de57630f00ecd3
mab839 <mab839@usask.ca>
2022-03-27 15:38:46 -0600

    Changed all the messages to be RadsatMessages and they now send over the UART debug line

--- radsat-sk/testing/admin/RMessageOutput.c
+++ radsat-sk/testing/admin/RMessageOutput.c
@@ -37,7 +37,7 @@ void sendAll(void) {
  * Prints an ACK message
  */
 void printAck(void) {
-	uint8_t wrappedMessage[RADSAT_SK_MAX_MESSAGE_SIZE];
+	uint8_t wrappedMessage[RADSAT_SK_MAX_MESSAGE_SIZE] = { 0 };
 	uint8_t size = protocolGenerate(ProtocolMessage_ack_tag, wrappedMessage);
 
 	uartTransmit(UART_DEBUG_BUS, wrappedMessage, size);
@@ -48,7 +48,7 @@ void printAck(void) {
  * Prints a NACK message
  */
 void sendNack(void) {
-	uint8_t wrappedMessage[RADSAT_SK_MAX_MESSAGE_SIZE];
+	uint8_t wrappedMessage[RADSAT_SK_MAX_MESSAGE_SIZE] = { 0 };
 	uint8_t size = protocolGenerate(ProtocolMessage_nack_tag, wrappedMessage);
 
 	uartTransmit(UART_DEBUG_BUS, wrappedMessage, size);
@@ -68,33 +68,33 @@ void sendFileTransferMessages(void) {
 	RadsatMessage mockDosimeterData;
 	mockDosimeterData.which_service = RadsatMessage_service_fileTransferMessage_MSGTYPE;
 
-	mockDosimeterData.boardOne.voltageChannelZero = 1.0;
-	mockDosimeterData.boardOne.voltageChannelOne = 1.1;
-	mockDosimeterData.boardOne.voltageChannelTwo = 1.2;
-	mockDosimeterData.boardOne.voltageChannelThree = 1.3;
-	mockDosimeterData.boardOne.voltageChannelFour = 1.4;
-	mockDosimeterData.boardOne.voltageChannelFive = 1.5;
-	mockDosimeterData.boardOne.voltageChannelSix = 1.6;
-	mockDosimeterData.boardOne.voltageChannelSeven = 1.7;
+	mockDosimeterData.fileTransferMessage.dosimeterData.boardOne.voltageChannelZero = 1.0;
+	mockDosimeterData.fileTransferMessage.dosimeterData.boardOne.voltageChannelOne = 1.1;
+	mockDosimeterData.fileTransferMessage.dosimeterData.boardOne.voltageChannelTwo = 1.2;
+	mockDosimeterData.fileTransferMessage.dosimeterData.boardOne.voltageChannelThree = 1.3;
+	mockDosimeterData.fileTransferMessage.dosimeterData.boardOne.voltageChannelFour = 1.4;
+	mockDosimeterData.fileTransferMessage.dosimeterData.boardOne.voltageChannelFive = 1.5;
+	mockDosimeterData.fileTransferMessage.dosimeterData.boardOne.voltageChannelSix = 1.6;
+	mockDosimeterData.fileTransferMessage.dosimeterData.boardOne.voltageChannelSeven = 1.7;
 
-	mockDosimeterData.boardTwo.voltageChannelZero = 2.0;
-	mockDosimeterData.boardTwo.voltageChannelOne = 2.1;
-	mockDosimeterData.boardTwo.voltageChannelTwo = 2.2;
-	mockDosimeterData.boardTwo.voltageChannelThree = 2.3;
-	mockDosimeterData.boardTwo.voltageChannelFour = 2.4;
-	mockDosimeterData.boardTwo.voltageChannelFive = 2.5;
-	mockDosimeterData.boardTwo.voltageChannelSix = 2.6;
-	mockDosimeterData.boardTwo.voltageChannelSeven = 2.7;
+	mockDosimeterData.fileTransferMessage.dosimeterData.boardTwo.voltageChannelZero = 2.0;
+	mockDosimeterData.fileTransferMessage.dosimeterData.boardTwo.voltageChannelOne = 2.1;
+	mockDosimeterData.fileTransferMessage.dosimeterData.boardTwo.voltageChannelTwo = 2.2;
+	mockDosimeterData.fileTransferMessage.dosimeterData.boardTwo.voltageChannelThree = 2.3;
+	mockDosimeterData.fileTransferMessage.dosimeterData.boardTwo.voltageChannelFour = 2.4;
+	mockDosimeterData.fileTransferMessage.dosimeterData.boardTwo.voltageChannelFive = 2.5;
+	mockDosimeterData.fileTransferMessage.dosimeterData.boardTwo.voltageChannelSix = 2.6;
+	mockDosimeterData.fileTransferMessage.dosimeterData.boardTwo.voltageChannelSeven = 2.7;
 
-	uint8_t wrappedMockDosimeterData[RADSAT_SK_MAX_MESSAGE_SIZE];
-	messageWrap(&mockDosimeterData, wrappedMockDosimeterData)
+	uint8_t wrappedMockDosimeterData[RADSAT_SK_MAX_MESSAGE_SIZE] = { 0 };
+	messageWrap(&mockDosimeterData, wrappedMockDosimeterData);
 
 	int dosimiterSize = fileTransferAddMessage(wrappedMockDosimeterData,
 												RADSAT_SK_MAX_MESSAGE_SIZE,
 												FileTransferMessage_dosimeterData_tag);
 
 	if (dosimiterSize != 0) {
-		uint8_t dosimeterFrame[RADSAT_SK_MAX_MESSAGE_SIZE];
+		uint8_t dosimeterFrame[RADSAT_SK_MAX_MESSAGE_SIZE] = { 0 };
 
 		int dosimeterFrameSize = fileTransferNextFrame(&dosimeterFrame);
 
@@ -113,17 +113,20 @@ void sendFileTransferMessages(void) {
 	// OBC Telemetry
 	RadsatMessage mockObcTelemetry;
 
-	mockObcTelemetry.mode = 0;
-	mockObcTelemetry.uptime = 1;
-	mockObcTelemetry.rtcTime = 2;
-	mockObcTelemetry.rtcTemperature = 3;
+	mockObcTelemetry.fileTransferMessage.obcTelemetry.mode = 0;
+	mockObcTelemetry.fileTransferMessage.obcTelemetry.uptime = 1;
+	mockObcTelemetry.fileTransferMessage.obcTelemetry.rtcTime = 2;
+	mockObcTelemetry.fileTransferMessage.obcTelemetry.rtcTemperature = 3;
+
+	uint8_t wrappedMockObcTelemetry[RADSAT_SK_MAX_MESSAGE_SIZE] = { 0 };
+	messageWrap(&mockObcTelemetry, wrappedMockObcTelemetry);
 
 	int obcTelemetrySize = fileTransferAddMessage(wrappedMockObcTelemetry,
 													RADSAT_SK_MAX_MESSAGE_SIZE,
 													FileTransferMessage_dosimeterData_tag);
 
 	if (obcTelemetrySize != 0) {
-		uint8_t obcTelemetryFrame[RADSAT_SK_MAX_MESSAGE_SIZE];
+		uint8_t obcTelemetryFrame[RADSAT_SK_MAX_MESSAGE_SIZE] = { 0 };
 
 		int obcTelemetryFrameSize = fileTransferNextFrame(&obcTelemetryFrame);
 
@@ -143,36 +146,39 @@ void sendFileTransferMessages(void) {
 	// Transceiver Telemetry
 	RadsatMessage mockTransceiverTelemetry;
 
-	mockTransceiverTelemetry.receiver.rx_doppler = 1.0;
-	mockTransceiverTelemetry.receiver.rx_rssi = 1.1;
-	mockTransceiverTelemetry.receiver.bus_volt = 1.2;
-	mockTransceiverTelemetry.receiver.vutotal_curr = 1.3;
-	mockTransceiverTelemetry.receiver.vutx_curr = 1.4;
-	mockTransceiverTelemetry.receiver.vurx_curr = 1.5;
-	mockTransceiverTelemetry.receiver.vupa_curr = 1.6;
-	mockTransceiverTelemetry.receiver.pa_temp = 1.7;
-	mockTransceiverTelemetry.receiver.board_temp = 1.8;
-	mockTransceiverTelemetry.receiver.uptime = 2;
-	mockTransceiverTelemetry.receiver.frames = 3;
+	mockTransceiverTelemetry.fileTransferMessage.transceiverTelemetry.receiver.rx_doppler = 1.0;
+	mockTransceiverTelemetry.fileTransferMessage.transceiverTelemetry.receiver.rx_rssi = 1.1;
+	mockTransceiverTelemetry.fileTransferMessage.transceiverTelemetry.receiver.bus_volt = 1.2;
+	mockTransceiverTelemetry.fileTransferMessage.transceiverTelemetry.receiver.vutotal_curr = 1.3;
+	mockTransceiverTelemetry.fileTransferMessage.transceiverTelemetry.receiver.vutx_curr = 1.4;
+	mockTransceiverTelemetry.fileTransferMessage.transceiverTelemetry.receiver.vurx_curr = 1.5;
+	mockTransceiverTelemetry.fileTransferMessage.transceiverTelemetry.receiver.vupa_curr = 1.6;
+	mockTransceiverTelemetry.fileTransferMessage.transceiverTelemetry.receiver.pa_temp = 1.7;
+	mockTransceiverTelemetry.fileTransferMessage.transceiverTelemetry.receiver.board_temp = 1.8;
+	mockTransceiverTelemetry.fileTransferMessage.transceiverTelemetry.receiver.uptime = 2;
+	mockTransceiverTelemetry.fileTransferMessage.transceiverTelemetry.receiver.frames = 3;
 
 
-	mockTransceiverTelemetry.transmitter.tx_reflpwr = 4.0;
-	mockTransceiverTelemetry.transmitter.tx_fwrdpwr = 4.1;
-	mockTransceiverTelemetry.transmitter.bus_volt = 4.2;
-	mockTransceiverTelemetry.transmitter.vutotal_curr = 4.3;
-	mockTransceiverTelemetry.transmitter.vutx_curr = 4.4;
-	mockTransceiverTelemetry.transmitter.vurx_curr = 4.5;
-	mockTransceiverTelemetry.transmitter.vupa_curr = 4.6;
-	mockTransceiverTelemetry.transmitter.pa_temp = 4.7;
-	mockTransceiverTelemetry.transmitter.board_temp = 4.8;
-	mockTransceiverTelemetry.transmitter.uptime = 5;
+	mockTransceiverTelemetry.fileTransferMessage.transceiverTelemetry.transmitter.tx_reflpwr = 4.0;
+	mockTransceiverTelemetry.fileTransferMessage.transceiverTelemetry.transmitter.tx_fwrdpwr = 4.1;
+	mockTransceiverTelemetry.fileTransferMessage.transceiverTelemetry.transmitter.bus_volt = 4.2;
+	mockTransceiverTelemetry.fileTransferMessage.transceiverTelemetry.transmitter.vutotal_curr = 4.3;
+	mockTransceiverTelemetry.fileTransferMessage.transceiverTelemetry.transmitter.vutx_curr = 4.4;
+	mockTransceiverTelemetry.fileTransferMessage.transceiverTelemetry.transmitter.vurx_curr = 4.5;
+	mockTransceiverTelemetry.fileTransferMessage.transceiverTelemetry.transmitter.vupa_curr = 4.6;
+	mockTransceiverTelemetry.fileTransferMessage.transceiverTelemetry.transmitter.pa_temp = 4.7;
+	mockTransceiverTelemetry.fileTransferMessage.transceiverTelemetry.transmitter.board_temp = 4.8;
+	mockTransceiverTelemetry.fileTransferMessage.transceiverTelemetry.transmitter.uptime = 5;
+
+	uint8_t wrappedMockTransceiverTelemetry[RADSAT_SK_MAX_MESSAGE_SIZE] = { 0 };
+	messageWrap(&mockTransceiverTelemetry, wrappedMockTransceiverTelemetry);
 
 	int transceiverTelemetrySize = fileTransferAddMessage(wrappedMockTransceiverTelemetry,
 															RADSAT_SK_MAX_MESSAGE_SIZE,
 															FileTransferMessage_dosimeterData_tag);
 
 	if (transceiverTelemetrySize != 0) {
-		uint8_t transceiverFrame[sizeof(mockTransceiverTelemetry)] = { 0 };
+		uint8_t transceiverFrame[RADSAT_SK_MAX_MESSAGE_SIZE] = { 0 };
 
 		int transceiverTelemetryFrameSize = fileTransferNextFrame(&transceiverFrame);
 
