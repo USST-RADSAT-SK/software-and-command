6178f52485216fe29a1275d9402c6c1158d2af03
Addi Amaya <chris.amaya.caa@gmail.com>
2022-04-02 16:41:08 -0600

    Cleaned up the code a bit and and finished telemetry function

--- radsat-sk/operation/subsystems/RCamera.h
+++ radsat-sk/operation/subsystems/RCamera.h
@@ -12,14 +12,7 @@
 /***************************************************************************************************
                                             DEFINITIONS
 ***************************************************************************************************/
-// Camera telecommand_ Define ID and the Length
-typedef struct _camera_telecommand_t {
-	uint16_t ID;
-	uint8_t paramLength;
-} camera_telecommand_t;
-
-/*Define parameters*/
-
+/* Struct used to define ADCS Function*/
 typedef struct _detection_results_t {
 	uint16_t sunSensorX;
 	uint16_t sunSensorY;
@@ -29,115 +22,32 @@ typedef struct _detection_results_t {
 	uint16_t imageSensorZ;
 } detection_results_t;
 
-// ResetTelecommand
-typedef struct _tc_reset_Telecommand_params_t {
-	uint8_t resetType;
-} tc_reset_Telecommand_params_t;
+typedef struct _CameraTelemetry_PowerTelemetry {
+    float current_3V3;
+    float current_5V;
+    float current_SRAM_1;
+    float current_SRAM_2;
+    uint8_t overcurrent_SRAM_1;
+    uint8_t overcurrent_SRAM_2;
+} CameraTelemetry_PowerTelemetry;
 
-// Clear SRAM overcurrent flags Telecommand
-typedef struct _tc_clear_SRAM_overcurrent_params_t {
-	uint8_t SRAMOverCurrentFlag;
-} tc_clear_SRAM_over_current_t;
+typedef struct _CameraTelemetry_ConfiguarationTelemetry {
+    uint8_t detectionThreshold;
+    uint8_t autoAdjustMode;
+    uint16_t exposure;
+    uint8_t autoGainControl;
+    uint8_t blueGain;
+    uint8_t redGain;
+} CameraTelemetry_ConfigurationTelemetry;
 
-// Image capture & detection Telecommand
-typedef struct _tc_image_capture_anddetect_params_t {
-	uint8_t camera;
-	uint8_t sram;
-} tc_image_capture_anddetect_params_t ;
+/* Struct for Camera Telemetry Collection Function */
+typedef struct _CameraTelemetry {
+    uint16_t upTime;
+    CameraTelemetry_PowerTelemetry powerTelemetry;
+    CameraTelemetry_ConfigurationTelemetry cameraOneTelemetry;
+    CameraTelemetry_ConfigurationTelemetry cameraTwoTelemetry;
+} CameraTelemetry;
 
-//Set sensor 1 detection threshold Telecommand
-typedef struct _tc_set_sensor_one_detection_threshold_t {
-	uint8_t detectionThreshold1;
-} tc_set_sensor_one_detection_threshold_t ;
-
-//Set sensor 2 detection threshold Telecommand
-typedef struct _tc_set_sensor_two_detection_threshold_t {
-	uint8_t detectionThreshold2;
-} tc_set_sensor_two_detection_threshold_t ;
-
-//Set sensor 1 auto-adjust Telecommand
-typedef struct _tc_set_SensorOne_Autoadjust_t {
-	uint8_t autoadjustenable1;
-} tc_set_SensorOne_Autoadjust_t ;
-
-//Set sensor 1 settings Telecommand
-typedef struct _tc_set_SensorOne_Setting_t {
-	uint8_t exposureTime1;
-	uint8_t AGC1;
-	uint8_t blueGain1;
-	uint8_t redGain1;
-} tc_set_SensorOne_Setting_t ;
-
-//Set sensor 2 auto-adjust Telecommand
-typedef struct _tc_set_SensorTwo_Autoadjust_t {
-	uint8_t autoadjustenable2;
-} tc_set_SensorTwo_Autoadjust_t ;
-
-//Set sensor 2 settings Telecommand
-typedef struct _tc_set_SensorTwo_Setting_t {
-	 uint8_t exposureTime2;
-	 uint8_t AGC2;
-	 uint8_t blueGain2;
-	 uint8_t redGain2;
-} tc_set_SensorTwo_Setting_t ;
-
-//Set sensor 1 boresight pixel location Telecommand
-typedef struct _tc_set_SensorOne_Boresight_Pixel_Location_t {
-	 uint16_t X_pixel1;
-	 uint16_t Y_pixel1;
-} tc_set_SensorOne_Boresight_Pixel_Location_t ;
-
-//Set sensor 2 boresight pixel location Telecommand
-typedef struct _tc_set_SensorTwo_Boresight_Pixel_Location_t {
-	  uint16_t X_pixel2;
-	  uint16_t Y_pixel2;
-} tc_set_SensorTwo_Boresight_Pixel_Location_t ;
-
-//Set sensor 1 mask Telecommand
-typedef struct _tc_set_SensorOne_Mask_t {
-	  uint8_t  maskNumber1;
-	  uint16_t xMinimum1;
-	  uint16_t xMaximum1;
-	  uint16_t yMinimum1;
-	  uint16_t yMaximum1;
-} tc_set_SensorOne_Mask_t;
-
-//Set sensor 2 mask Telecommand
-typedef struct _tc_set_SensorTwo_Mask_t {
-	  uint8_t  maskNumber2;
-	  uint16_t xMinimum2;
-	  uint16_t xMaximum2;
-	  uint16_t yMinimum2;
-	  uint16_t yMaximum2;
-} tc_set_SensorTwo_Mask_t;
-
-//Set sensor 1 distortion correction coefficients Telecommand
-typedef struct _tc_set_SensorOne_distortionCorrectionCoefficient_params_t {
-	   uint16_t Mantissa11;
-	   uint8_t  Exponent11;
-	   uint16_t Mantissa12;
-	   uint8_t  Exponent12;
-	   uint16_t Mantissa13;
-	   uint8_t  Exponent13;
-	   uint16_t Mantissa14;
-	   uint8_t  Exponent14;
-	   uint16_t Mantissa15;
-	   uint8_t  Exponent15;
-} tc_set_SensorOne_distortionCorrectionCoefficient_params_t;
-
-//Set sensor 2 distortion correction coefficients Telecommand
-typedef struct _tc_set_SensorTwo_distortionCorrectionCoefficient_t {
-	   uint16_t Mantissa21;
-	   uint8_t  Exponent21;
-	   uint16_t Mantissa22;
-	   uint8_t  Exponent22;
-	   uint16_t Mantissa23;
-	   uint8_t  Exponent23;
-	   uint16_t Mantissa24;
-	   uint8_t  Exponent24;
-	   uint16_t Mantissa25;
-	   uint8_t  Exponent25;
-} tc_set_SensorTwo_distortionCorrectionCoefficient_t;
 
 
 /* Struct for telmetry status, ID 0*/
@@ -150,11 +60,6 @@ typedef struct _tlm_status_t {
 	uint16_t runtimeMSeconds;
 } tlm_status_t;
 
-/* Struct for telmetry Serial number, ID 1 */
-typedef struct _tlm_serial_number_t {
-	uint64_t Nodetype;
-} tlm_serial_number_t;
-
 /* Struct for telmetry Serial number, ID 2 */
 typedef struct _tlm_communication_status_t {
 	uint16_t tcCounter;
@@ -164,14 +69,6 @@ typedef struct _tlm_communication_status_t {
 	uint8_t	 uarttlmProtocolErrorFlag;
 	uint8_t  uartIncompleteMsgFlag;
 } tlm_communication_status_t;
-
-/* Struct for telemetry telecommand acknowledege, ID 3*/
-typedef struct _tlm_telecommand_ack_t {
-	uint8_t last_tc_id;
-	uint8_t processed_flag;
-	uint8_t tc_error_flag;
-} tlm_telecommand_ack_t;
-
 
 /* Struct for telemetry power, ID 26 */
 typedef struct _tlm_power_t {
@@ -198,7 +95,6 @@ typedef struct _tlm_config_t{
 	uint8_t cameraTwoBlueGain;
 	uint8_t cameraTwoRedGain;
 } tlm_config_t;
-
 
 /* Struct for telemetry full image, ID 66-69 */
 typedef struct _tlm_full_image_t {
@@ -233,4 +129,7 @@ typedef struct _tlm_read_sensor_mask_t {
                                              PUBLIC API
 ***************************************************************************************************/
 int captureAndDownload(void);
+int detectionAndInterpret(detection_results_t *data);
+int cameraTelemetry(CameraTelemetry *cameraTelemetry);
+
 #endif /* RCAMERA_H_ */
