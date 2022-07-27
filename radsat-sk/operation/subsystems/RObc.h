f59395727387a38b3b98e1aff064125270d05c00
Austin Hruska <Austin.Hruska@usask.ca>
2022-07-27 13:02:07 -0600

    Incompleate implementation of extra telemetry

--- radsat-sk/operation/subsystems/RObc.h
+++ radsat-sk/operation/subsystems/RObc.h
@@ -10,12 +10,38 @@
 #include <stdlib.h>
 
 
+
+
 /***************************************************************************************************
                                             DEFINITIONS
 ***************************************************************************************************/
 
+// 255 is index for SPI communication
+#define SUPERVISOR_INDEX	255
+
+/** The number of channels used in the Supervisor Controller. */
+#define SUPERVISOR_NUMBER_OF_ADC_CHANNELS	10
+
+
 typedef struct _obc_telemetry_t {
 	float temperature;
+	int16_t adcValue[SUPERVISOR_NUMBER_OF_ADC_CHANNELS];
+	uint32_t supervisorUptime;
+	uint32_t IobcUptime;
+	uint32_t IobcPowerCycleCount;
+
+	temperature;
+
+	int16_t voltage3v3In;
+	int16_t voltageRef;
+	int16_t voltageRtc;
+	int16_t voltage3v3;
+	int16_t voltage1v8;
+	int16_t voltage1v0;
+
+	int16_t current3v3;
+	int16_t current1v8;
+	int16_t current1v0;
 } obc_telemetry_t;
 
 
