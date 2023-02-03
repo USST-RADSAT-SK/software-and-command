f7a94337158befd3434e0e475d1170a1b1e30d96
Austin <austin.hruska@usask.ca>
2023-02-03 05:54:29 -0600

    Adding battery test file

---
+++ radsat-sk/testing/unit/RTestBattery.h
@@ -0,0 +1,21 @@
+/**
+ * @file RTestBattery.h
+ * @date January 30, 2023
+ * @author Austin Hruska (jah385)
+ */
+
+#ifndef RTESTBATTERY_H_
+#define RTESTBATTERY_H_
+
+
+
+/***************************************************************************************************
+                                             PUBLIC API
+***************************************************************************************************/
+
+int testSelectBattery(unsigned int autoSelection);
+int checkBattery(void);
+int testBatteryAll(void);
+
+
+#endif /* RTESTBATTERY_H_ */
